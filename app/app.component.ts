78d0f79dd033104f0349a8282475fc58da17c66b
David Sudia <dsudia@gmail.com>
2016-05-30 16:25:28 -0600

    added some more structural files and get basic html on landing page

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,32 +2,14 @@ import {Component} from "@angular/core";
 import {RouteConfig} from "@angular/router-deprecated";
 import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 import {LandingPage} from "./pages/landing/landing.component";
-import {HTTP_PROVIDERS} from "@angular/http";
 
 @Component({
-    selector: "my-app",
-    template: `
-<StackLayout>
-    <Label text="Tap the button" class="title"></Label>
-    
-    <Button text="TAP" (tap)="onTap()"></Button>
-
-    <Label [text]="message" class="message" textWrap="true"></Label>
-</StackLayout>
-`,
+    selector: "main",
+    directives: [NS_ROUTER_DIRECTIVES],
+    providers: [NS_ROUTER_PROVIDERS],
+    template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-    public counter: number = 16;
-
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
-    public onTap() {
-        this.counter--;
-    }
-}
+@RouteConfig([
+    { path: "/Landing", component: LandingPage, name: "Landing", useAsDefault: true}
+])
+export class AppComponent {}
